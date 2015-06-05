445240240f3f786dad67f00d7093ca2b4806b20a
Jason Hatchett <jasonh@jasonh-mbp.spice.spiceworks.com>
2015-06-05 13:51:57 -0500

    Selection and Randomization Occurs

--- avalon_platform.js
+++ avalon_platform.js
@@ -1,28 +1,34 @@
 if (Meteor.isClient) {
 
   Session.set('players', [])
+  Session.set('roles', [])
+  Session.set('assigns', [])
 
   Template.body.helpers({
 
     cards:[
-      {role:'Merlin'                  , evil:false, selected:false},
-      {role:'Percival'                , evil:false, selected:false},
-      {role:'Loyal Servant of Arthur' , evil:false, selected:false}, 
-      {role:'Loyal Servant of Arthur' , evil:false, selected:false},
-      {role:'Loyal Servant of Arthur' , evil:false, selected:false},
-      {role:'Loyal Servant of Arthur' , evil:false, selected:false},
-      {role:'Loyal Servant of Arthur' , evil:false, selected:false},
-      {role:'Assassin'                , evil:true , selected:false},
-      {role:'Morgana'                 , evil:true , selected:false},
-      {role:'Mordred'                 , evil:true , selected:false},
-      {role:'Oberon'                  , evil:true , selected:false},
-      {role:'Minion of Mordred'       , evil:true , selected:false},
-      {role:'Minion of Mordred'       , evil:true , selected:false},
-      {role:'Minion of Mordred'       , evil:true , selected:false}
+      {id: 1 , role:'Merlin'                  , evil:false},
+      {id: 2 , role:'Percival'                , evil:false},
+      {id: 3 , role:'Loyal Servant of Arthur' , evil:false}, 
+      {id: 4 , role:'Loyal Servant of Arthur' , evil:false},
+      {id: 5 , role:'Loyal Servant of Arthur' , evil:false},
+      {id: 6 , role:'Loyal Servant of Arthur' , evil:false},
+      {id: 7 , role:'Loyal Servant of Arthur' , evil:false},
+      {id: 8 , role:'Assassin'                , evil:true},
+      {id: 9 , role:'Morgana'                 , evil:true},
+      {id: 10, role:'Mordred'                 , evil:true},
+      {id: 11, role:'Oberon'                  , evil:true},
+      {id: 12, role:'Minion of Mordred'       , evil:true},
+      {id: 13, role:'Minion of Mordred'       , evil:true},
+      {id: 14, role:'Minion of Mordred'       , evil:true}
     ],
 
     players: function (){
       return Session.get('players')
+    },
+
+    assigns: function(){
+      return Session.get('assigns')
     }
 
     //change css of cards based on selected?
@@ -44,8 +50,60 @@ if (Meteor.isClient) {
         // Clear text
         event.target.value = "";
       }
-  }
+    },
+    "click .bttn": function (event){
+      var r = Session.get('roles');
+      var p = Session.get('players');
+      var as= [];
+      //check if player count equals roles count
+      if (r.length == p.length){
+         //randomize and display
+         r = shuffle(r);
+         p = shuffle(p);
+         for (var i = 0; i < r.length; i++){
+          as.push({"player": p[i], "role": r[i]})
+         }
+         Session.set('assigns', as);
+      }
+    }
+
   });
+
+  Template.roles.events({
+  "change":function(event){
+    var array = Session.get('roles');
+    var item = event.target.getAttribute('iden');
+    var idx = array.indexOf(item)
+    if (idx < 0){
+      array.push(item);
+      Session.set('roles', array);
+    }
+    else{
+      array.splice(idx, 1);
+      Session.set('roles', array);
+    }
+  }
+  })
+
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex ;
+  
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+  
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+  
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+  
+    return array;
+  }
+
 }
 
 if (Meteor.isServer) {
